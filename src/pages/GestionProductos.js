f6dce81c7c8891a58a6d086e19b086fe0a64d9fa
Valerin Calderon <kbran1409@gmail.com>
2023-05-01 15:25:48 -0600

    Update GestionProductos.js

--- src/pages/GestionProductos.js
+++ src/pages/GestionProductos.js
@@ -3,52 +3,56 @@ import "../styles/Login.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import DataTable from "react-data-table-component";
 
-const GestionProductos = () => {
+const GestionProductos=() =>{
 
 
-    const tablaProductos = [
+    const tablaProductos =[
 
-        { Nombre: 'Terapia', Código: '1233', Descripción: 'Masajes', Costos: '5000' }];
-
-    const columnas = [
+        {Nombre: 'Terapia', Código:'1233', Descripción:'Masajes', Costos:'5000'}];
+        
+        const columnas =[
         {
-            name: 'Nombre',
-            selector: 'Nombre',
-            sortable: true
+        name: 'Nombre',
+        selector: 'Nombre',
+        sortable: true
         },
         {
             name: 'Código',
             selector: 'Código',
             sortable: true
-        },
-        {
+            },
+         {
             name: 'Descripción',
             selector: 'Descripción',
             sortable: true
-        },
-        {
+            },
+         {
             name: 'Costo',
             selector: 'Costos',
             sortable: true
+            }
+        ];
+        const paginacionOpciones={
+            rowsPerPageText: 'Filas por Página',
+            rangeSeparatorText: 'de',
+            selectAllRowsItem: true,
+            selectAllRowsItemText: 'todos'
+        
         }
-    ];
-    const paginacionOpciones = {
-        rowsPerPageText: 'Filas por Página',
-        rangeSeparatorText: 'de',
-        selectAllRowsItem: true,
-        selectAllRowsText: 'todos'
+        
+        return (
+        <div className="table-responsive">
+        <DataTable
+        columns={columnas}
+        data={tablaProductos}
+        title='Productos por sucursal'
+        pagination
+        paginationComponent={paginacionOpciones}
+        /> 
+        
+        </div>)
 
-    }
 
-    return (
-        <div>
-            <DataTable
-                columns={columnas}
-                data={tablaProductos}
-                title='Productos por sucursal' />
-
-        </div>
-    )
 }
 
 
