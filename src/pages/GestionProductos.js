72f795f412dad82751d3a8107e0195bf56bb5a33
Valerin Calderon <76504802+VKCB@users.noreply.github.com>
2023-05-01 15:06:38 -0600

    Update GestionProductos.js

---
+++ src/pages/GestionProductos.js
@@ -0,0 +1,56 @@
+import React from "react";
+import "../styles/Login.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import DataTable from "react-data-table-component";
+
+const GestionProductos=() =>{
+
+
+    const tablaProductos =[
+
+        {Nombre: 'Terapia', Código:'1233', Descripción:'Masajes', Costos:'5000'}];
+        
+        const columnas =[
+        {
+        name: 'Nombre',
+        selector: 'Nombre',
+        sortable: true
+        },
+        {
+            name: 'Código',
+            selector: 'Código',
+            sortable: true
+            },
+         {
+            name: 'Descripción',
+            selector: 'Descripción',
+            sortable: true
+            },
+         {
+            name: 'Costo',
+            selector: 'Costos',
+            sortable: true
+            }
+        ];
+        const paginacionOpciones={
+            rowsPerPageText: 'Filas por Página',
+            rangeSeparatorText: 'de',
+            selectAllRowsItem: true,
+            selectAllRowsText: 'todos'
+        
+        }
+        
+        return (
+        <div>
+        <DataTable
+        columns={columnas}
+        data={tablaProductos}
+        title='Productos por sucursal'/> 
+        
+        </div>)
+
+
+}
+
+
+export default GestionProductos;
