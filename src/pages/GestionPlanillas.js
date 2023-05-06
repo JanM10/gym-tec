65499dde4425f63711a5d850209c7e6f68649067
MrWrynn <64748923+MrWrynn@users.noreply.github.com>
2023-05-05 21:02:47 -0600

    fixes and gestionPlanillas commit

--- src/pages/GestionPlanillas.js
+++ src/pages/GestionPlanillas.js
@@ -1,60 +1,60 @@
 import React, { useState } from 'react';
 import { Table, Button, Modal, Form } from 'react-bootstrap';
 
-const GestionServicios = () => {
-    const [servicios, setServicios] = useState([]);
+const GestionPlanillas = () => {
+    const [planillas, setPlanillas] = useState([]);
     const [descripcion, setDescripcion] = useState('');
-    const [servicioId, setServicioId] = useState(null);
+    const [planillaId, setPlanillaId] = useState(null);
     const [showModal, setShowModal] = useState(false);
 
-    const agregarServicio = () => {
+    const agregarPlanilla = () => {
         if (!descripcion) {
-            alert('Debes completar la descripción del servicio');
+            alert('Debes completar la descripción del planilla');
             return;
         }
-        const nuevoServicio = { id: servicios.length + 1, descripcion };
-        setServicios([...servicios, nuevoServicio]);
+        const nuevoPlanilla = { id: planillas.length + 1, descripcion };
+        setPlanillas([...planillas, nuevoPlanilla]);
         setDescripcion('');
     };
 
-    const eliminarServicio = (id) => {
-        const serviciosActualizados = servicios.filter(servicio => servicio.id !== id);
-        setServicios(serviciosActualizados);
+    const eliminarPlanilla = (id) => {
+        const planillasActualizados = planillas.filter(planilla => planilla.id !== id);
+        setPlanillas(planillasActualizados);
     };
 
-    const editarServicio = () => {
+    const editarPlanilla = () => {
         if (!descripcion) {
-            alert('Debes completar la descripción del servicio');
+            alert('Debes completar la descripción del planilla');
             return;
         }
-        const serviciosActualizados = servicios.map(servicio => {
-            if (servicio.id === servicioId) {
-                return { ...servicio, descripcion };
+        const planillasActualizados = planillas.map(planilla => {
+            if (planilla.id === planillaId) {
+                return { ...planilla, descripcion };
             }
-            return servicio;
+            return planilla;
         });
-        setServicios(serviciosActualizados);
+        setPlanillas(planillasActualizados);
         setDescripcion('');
-        setServicioId(null);
+        setPlanillaId(null);
         setShowModal(false);
     };
 
     const handleEditar = (id) => {
-        const servicioEncontrado = servicios.find(servicio => servicio.id === id);
-        setDescripcion(servicioEncontrado.descripcion);
-        setServicioId(id);
+        const planillaEncontrado = planillas.find(planilla => planilla.id === id);
+        setDescripcion(planillaEncontrado.descripcion);
+        setPlanillaId(id);
         setShowModal(true);
     };
 
     const handleModalClose = () => {
         setShowModal(false);
         setDescripcion('');
-        setServicioId(null);
+        setPlanillaId(null);
     };
 
     return (
         <div className="container">
-            <h2 className="my-4">Gestión de Servicios</h2>
+            <h2 className="my-4">Gestión de Planillas</h2>
             <div className="row mb-3">
                 <div className="col-md-6">
                     <label htmlFor="descripcion" className="form-label">Descripción:</label>
@@ -68,10 +68,10 @@ const GestionServicios = () => {
                     <small>{descripcion.length}/50 caracteres</small>
                 </div>
                 <div className="col-md-3 d-flex align-items-end">
-                    {servicioId === null ? (
-                        <Button variant="primary" onClick={agregarServicio}>Agregar Servicio</Button>
+                    {planillaId === null ? (
+                        <Button variant="primary" onClick={agregarPlanilla}>Agregar Planilla</Button>
                     ) : (
-                        <Button variant="primary" onClick={() => setShowModal(true)}>Editar Servicio</Button>
+                        <Button variant="primary" onClick={() => setShowModal(true)}>Editar Planilla</Button>
                     )}
                 </div>
             </div>
@@ -84,13 +84,13 @@ const GestionServicios = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {servicios.map((servicio, index) => (
+                    {planillas.map((planilla, index) => (
                         <tr key={index}>
-                            <td>{servicio.id}</td>
-                            <td>{servicio.descripcion}</td>
+                            <td>{planilla.id}</td>
+                            <td>{planilla.descripcion}</td>
                             <td>
-                                <Button variant="danger" onClick={() => eliminarServicio(servicio.id)}>Eliminar</Button>
-                                <Button variant="primary" onClick={() => handleEditar(servicio.id)}>Editar</Button>
+                                <Button variant="danger" onClick={() => eliminarPlanilla(planilla.id)}>Eliminar</Button>
+                                <Button variant="primary" onClick={() => handleEditar(planilla.id)}>Editar</Button>
                             </td>
                         </tr>
                     ))}
@@ -99,7 +99,7 @@ const GestionServicios = () => {
 
             <Modal show={showModal} onHide={handleModalClose}>
                 <Modal.Header closeButton>
-                    <Modal.Title>Editar Servicio</Modal.Title>
+                    <Modal.Title>Editar Planilla</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <Form>
@@ -111,11 +111,11 @@ const GestionServicios = () => {
                 </Modal.Body>
                 <Modal.Footer>
                     <Button variant="secondary" onClick={handleModalClose}>Cancelar</Button>
-                    <Button variant="primary" onClick={editarServicio}>Guardar Cambios</Button>
+                    <Button variant="primary" onClick={editarPlanilla}>Guardar Cambios</Button>
                 </Modal.Footer>
             </Modal>
         </div>
     );
 };
 
-export default GestionServicios;
+export default GestionPlanillas;
